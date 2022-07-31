a51a763ddbd157ebd9cb5426c3a70004ca5e4ccd
elementalkey <41343085+nomarolivas@users.noreply.github.com>
2022-07-30 17:15:58 -0700

    Added SQLite Database

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-
         while (runProgram) {
             //Start Menu.
             Scanner userInput = new Scanner(System.in);
